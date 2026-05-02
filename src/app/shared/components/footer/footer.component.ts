2f819d167a6ec42810efcd7424a1ef32fd7b4ff2
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2026-05-02 09:19:49 +0300

    feat: create footer component

---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import { LinkComponent } from '../link/link.component';
+
+@Component({
+  selector: 'app-footer',
+  imports: [LinkComponent],
+  templateUrl: './footer.component.html',
+})
+export class FooterComponent {}
